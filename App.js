800066dae983483d6784dfa071223e04f71c2091
Krish Bansal <73291597+Krish-002@users.noreply.github.com>
2024-04-22 03:37:20 -0400

    post

--- App.js
+++ App.js
@@ -5,12 +5,32 @@ import PostRoutes from "./Spot/Post/routes.js";
 import GymStatisticRoutes from "./Spot/GymStatistic/routes.js";
 import GymSplitRoutes from "./Spot/GymSplit/routes.js";
 import cors from "cors";
+
+// MongoDB connection
 mongoose.connect("mongodb+srv://sreekar:mongo@cluster79962.trmwwmq.mongodb.net/spot_social");
+
 const app = express();
-app.use(cors());
+
+// Define CORS options
+const corsOptions = {
+    origin: 'http://localhost:3000', // This should match the URL of your front-end app
+    credentials: true, // Allowing credentials (cookies, authorization headers, etc.)
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Use CORS with the specified options
+app.use(cors(corsOptions));
+
+// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+
+// Apply routes
 PostRoutes(app);
 GymStatisticRoutes(app);
 GymSplitRoutes(app);
 UserRoutes(app);
-app.listen(process.env.PORT || 4000)
+
+// Start server
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server running on port ${process.env.PORT || 4000}`);
+});
